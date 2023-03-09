b1c8ca159d14a5a3d2450e5720c67a63f0a871f5
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-03-09 20:09:53 +0400

    Add dark mode to the modal

--- src/components/TodoList/Modal.jsx
+++ src/components/TodoList/Modal.jsx
@@ -17,16 +17,16 @@ const Modal = () => {
 
   return (
     //modal container
-    <div className="flex items-center justify-center bg-blue-700/60 fixed z-50 inset-0">
+    <div className="flex items-center justify-center bg-blue-700/60 fixed z-50 inset-0 dark:bg-gray-900/80">
       {/* modal */}
-      <div className="bg-cyan-200 w-5/6 sm:w-2/3 md:w-1/2 lg:w-1/3 rounded-br-3xl rounded-tl-3xl shadow-2xl p-5">
-        <h1 className=" text-custom-blue text-4xl font-black italic drop-shadow-2xl text-center">
+      <div className="bg-cyan-200 w-5/6 sm:w-2/3 md:w-1/2 lg:w-1/3 rounded-br-3xl rounded-tl-3xl shadow-2xl p-5 dark:bg-slate-800 dark:shadow-cyan-500	dark:shadow-md ">
+        <h1 className=" text-custom-blue text-4xl font-black italic drop-shadow-2xl text-center dark:text-custom-white">
           Edit todo
         </h1>
         <div className="py-5 space-y-5 space-x-2">
           <input
             type="text"
-            className="border-b-4 border-custom-blue rounded-lg px-3 py-2 outline-blue-500 w-full"
+            className="border-b-4 border-custom-blue rounded-lg px-3 py-2 outline-blue-500 w-full dark:text-custom-white dark:bg-slate-700 dark:border-custom-white"
             placeholder="Edit todo"
             value={todo}
             onChange={(e) => {
@@ -37,7 +37,9 @@ const Modal = () => {
 
           <div className="flex justify-around pt-3">
             <button
-              className="bg-gray-600 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-gray-800 hover:border-gray-600 rounded duration-200"
+              className="bg-gray-600 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-gray-800 hover:border-gray-600 rounded duration-200
+              dark:bg-gray-500 dark:hover:bg-gray-400 dark:border-gray-700 dark:hover:border-gray-500
+              "
               onClick={() => dispatch(closeModal())}
             >
               Cancel
